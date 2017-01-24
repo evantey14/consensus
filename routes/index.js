d7d948dac3a4ed28a32b5bd5d31bca3c58180bcf
Kelvin Lu <kelvinlu@mit.edu>
2017-01-24 13:57:12 -0500

    hotfix - hopefully this solves everything

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,6 @@
 var express = require('express')
 var router = express.Router();
 
-var authController = require('../controllers/auth-controller'),
-    authenticate = authController.authenticateUser;
-
-
-
-
 /* GET home page. */
 router.get('/', function(req, res, next){
     res.render('index');
