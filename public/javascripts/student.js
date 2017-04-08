38fc6bd7d0f288c1ecf02cfc34afc9c3f9c2f18b
Jynnie <jynnie@mit.edu>
2017-04-08 11:44:09 -0400

    Adding jquery to student mockup

--- public/javascripts/student.js
+++ public/javascripts/student.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-  
+  //unclear
 });
+
+function amConfuse() {
+  console.log("Clicked");
+  $("#confused").toggleClass("disabled");
+  $("#not-confused").toggleClass("toggled");
+}
+
+function notConfuse() {
+  $("#confused").toggleClass("disabled");
+  $("#not-confused").toggleClass("toggled");
+}
