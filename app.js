aa4de25b2550958c53fd9144f03ce7a2919b6909
Kelvin Lu <kelvinlu@mit.edu>
2017-05-03 20:31:37 -0400

    error

--- app.js
+++ app.js
@@ -58,10 +58,10 @@ if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     console.log(err);
     res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+    // res.render('error', {
+    //   message: err.message,
+    //   error: err
+    // });
   });
 }
 
