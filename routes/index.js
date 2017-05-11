9a7c73ea03631642119782f9e142d8e577d407f2
Kelvin Lu <kelvinlu@mit.edu>
2017-05-10 20:18:19 -0400

    stuff

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express')
 var router = express.Router();
+var Room = require('../models/Room.js');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next){
     res.render('index');
+});
+
+router.post('/create', function(req, res, next){
+  var name = req.body.name;
+  Room.createRoom(name, function(err){
+    if(err){
+      return next(err);
+    }
+    res.send("DONE");
+  });
 });
 
 module.exports = router;
